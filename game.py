4a958c57fc3d3b4ba24da7b0ca12529cccf3dfc4
Hackbright Student <no-reply@hackbrightacademy.com>
2017-05-23 12:32:32 -0700

    Greeting player

---
+++ game.py
@@ -0,0 +1,15 @@
+"""A number-guessing game."""
+
+# greet player
+# get player name
+# choose random number between 1 and 100
+# repeat forever:
+#     get guess
+#     if guess is incorrect:
+#         give hint
+#         increase number of guesses
+#     else:
+#         congratulate player
+
+name = raw_input("Welcome, what is your name?")
+print "Hello, %s, do you want to play a guessing game?" %(name)
