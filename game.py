8562f7c5120bf30d7d279a496e9083c17304a9c2
Hackbright Student <no-reply@hackbrightacademy.com>
2017-05-23 12:58:39 -0700

    "Added all the syntax"

--- game.py
+++ game.py
@@ -11,5 +11,21 @@
 #     else:
 #         congratulate player
 
-name = raw_input("Welcome, what is your name?")
-print "Hello, %s, do you want to play a guessing game?" %(name)
+import random
+
+name = raw_input("Welcome, what is your name? ")
+print "Hello, %s, do you want to play a guessing game? " %(name)
+
+random_number = random.randint(1,101)
+user_guess = 0
+count = 0
+while user_guess != random_number:
+    user_guess = int(raw_input("Guess a number from 1 to 100: "))
+    if user_guess > random_number:
+        print "Your guess is too high!"
+        count += 1
+    elif user_guess < random_number:
+        print "Your guess is too low!"
+        count += 1
+    else:
+        print "Well done, %s, You found the number in %s trials!" %(name,count)
