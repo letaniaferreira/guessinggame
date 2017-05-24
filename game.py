d7a7eb25c6449f78a34f31d50eecb9da117b673f
Hackbright Student <no-reply@hackbrightacademy.com>
2017-05-23 17:08:46 -0700

    try except

--- game.py
+++ game.py
@@ -16,16 +16,27 @@ import random
 name = raw_input("Welcome, what is your name? ")
 print "Hello, %s, do you want to play a guessing game? " %(name)
 
-random_number = random.randint(1,101)
+random_number = random.randint(1,100)
 user_guess = 0
 count = 0
 while user_guess != random_number:
-    user_guess = int(raw_input("Guess a number from 1 to 100: "))
+    user_guess = raw_input("Guess a number from 1 to 100: ")
+    try:
+        user_guess = int(user_guess)
+        break
+    except ValueError:
+        print("Oops! That was an invalid number. Rounding number.")
+        user_guess = int(float(user_guess))
+        print user_guess
+
     if user_guess > random_number:
         print "Your guess is too high!"
         count += 1
     elif user_guess < random_number:
         print "Your guess is too low!"
+        count += 1
+    elif user_guess < 1 or user_guess > 100:
+        print "Please enter a number between 1 and 100."
         count += 1
     else:
         print "Well done, %s, You found the number in %s trials!" %(name,count)
